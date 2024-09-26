3875227b70a5e29f03ed1336930162a61d3910cd
rlaehddud475 <6070kjh475@naver.com>
2024-09-26 16:17:58 +0900

    20240926

--- react104/src/App.tsx
+++ react104/src/App.tsx
@@ -1,14 +1,5 @@
-import ButtonLayout from "./ButtonLayout";
-import CircleLayout from "./CircleLayout";
-
 function App() {
-  return (
-    <>
-      <ButtonLayout repeatNumber={3} />
-
-      <CircleLayout repeatNumber={5} />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
